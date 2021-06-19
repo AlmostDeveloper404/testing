8885e0fedec8bdb657ead1cc524371dc064c77c8
Compadre <vadimonishchyk@yandex.ru>
2021-06-19 12:57:02 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,world again!");
+            int a = 5;
+            int b = 10;
+            int res = b / a;
+            Console.WriteLine(res);
+        }
+    }
+}
